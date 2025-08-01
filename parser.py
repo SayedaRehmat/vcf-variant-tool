344943e8bd9ded330223be15cac9be0570fc7b00
SayedaRehmat <bc190408541@vu.edu.pk>
2025-08-01 08:39:07 -0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,16 @@
+from cyvcf2 import VCF
+
+def parse_vcf(path):
+    vcf = VCF(path)
+    variant_list = []
+    for var in vcf:
+        variant = {
+            'chrom': var.CHROM,
+            'pos': var.POS,
+            'ref': var.REF,
+            'alt': var.ALT[0],
+            'qual': var.QUAL
+        }
+        variant_list.append(variant)
+    return variant_list
 
