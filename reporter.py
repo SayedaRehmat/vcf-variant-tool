3f27438a8091213463b2846909aede0c198006df
SayedaRehmat <bc190408541@vu.edu.pk>
2025-08-01 08:39:51 -0700

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -1,1 +1,5 @@
+def print_report(variants):
+    print("CHROM\tPOS\tREF\tALT\tQUAL\tClinVar")
+    for v in variants:
+        print(f"{v['chrom']}\t{v['pos']}\t{v['ref']}\t{v['alt']}\t{v['qual']}\t{v['clinvar']}")
 
