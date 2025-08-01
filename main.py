2a6842e57e9b24d84149913acbe923882def4536
SayedaRehmat <bc190408541@vu.edu.pk>
2025-08-01 08:38:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from src.parser import parse_vcf
+from src.annotator import annotate_variant
+from src.reporter import print_report
+
+vcf_path = "data/test_sample.vcf"
+
+variants = parse_vcf(vcf_path)
+
+for var in variants:
+    annotation = annotate_variant(var)
+    var['clinvar'] = annotation
+
+print_report(variants)
 
